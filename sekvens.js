4b28f02971cec5b6c3fd2630ce5a0b0d1019099d
oliv1619 <oliv1619@stud.kea.dk>
2018-04-13 08:40:20 +0200

    grønt lys

---
+++ sekvens.js
@@ -0,0 +1,53 @@
+// siden er loadet: LorenzoKører
+$(window).on("load", lorenzoKører);
+
+/*
+Elementer
+Scene
+Baggrund
+LorenzoScooter
+Lysskift
+Mogens
+
+Animationer:
+kør
+stop
+vent
+kør
+stop
+sur
+review
+selvmord
+stop
+
+Lyde:
+*/
+
+function lorenzoKører() {
+    console.log("lorenzoKører");
+}
+
+//Start anim: kør
+//Stop anim: kør
+//Start anim: vent
+
+function grøntLys() {
+    console.log("grøntLys");
+}
+
+//Start anim: grønt
+//start anim: kør
+//Start anim: stop
+
+function surMogens() {
+    console.log("surMogens");
+}
+
+//Start anim sur
+//Start anim: review
+
+function Selvmord() {
+    console.log("Selvmord");
+    //Start anim: selvmord
+    //Start anim: Selvmord
+}
